26c42617a371789d9041afac16bb21b544756d4a
Adam Dyson <adam.dyson@outlook.com>
2018-05-22 23:19:34 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
-# Verify the installed Kong location.
+# Verify the installed location.
 docker exec --tty ${container_id} env TERM=xterm which kong
-
-# Verify the installed Kong version.
+# Verify the installed version.
 docker exec --tty ${container_id} env TERM=xterm kong version
-
-# Verify the health of Kong services.
+# Verify the health of services.
 docker exec --tty ${container_id} env TERM=xterm kong health
