ac15cb543254e277f22d8fb51f8b238804ba5141
Adam Dyson <adam.dyson@outlook.com>
2018-08-14 15:44:52 +1000

    Moved API verification calls into the playbook itself.

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -8,9 +8,3 @@ docker exec --tty ${container_id} env TERM=xterm which kong
 docker exec --tty ${container_id} env TERM=xterm kong version
 # Verify the health of services.
 docker exec --tty ${container_id} env TERM=xterm kong health
-# Expecting 401
-docker exec --tty ${container_id} env TERM=xterm curl -i -X GET --url http://localhost:8000 --header "Host: example.com"
-# Expecting 201
-docker exec --tty ${container_id} env TERM=xterm curl -i -X POST --url http://localhost:8001/consumers/adam/key-auth/ --data "key=SECRET_KEY"
-# Expecting 200
-docker exec --tty ${container_id} env TERM=xterm curl -i -X GET --url http://localhost:8000 --header "Host: example.com" --header "apikey: SECRET_KEY"
